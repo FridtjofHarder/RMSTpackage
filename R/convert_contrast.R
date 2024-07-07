823b09b76a28cc0e69317d7a04f85c852082a97f
FridtjofHarder <harder.fridtjof@gmail.com>
2024-07-08 00:03:37 +0200

    2nd commit 07.07.2024

--- R/convert_contrast.R
+++ R/convert_contrast.R
@@ -19,9 +19,11 @@
 #' @param percentile_diff A scalar specifying the difference in time t \eqn{\Delta t} at which each group has the survival \eqn{S(t) =} \code{percentile}.
 #' @param percentile A scalar specifying at which percentile to evaluate \code{percentile_diff}.
 #' @param survival_diff A scalar specifying the survival difference in survival \eqn{\Delta S(t)} between treatment and control group.
-#' @param output A string specifying whether to calculate the difference in RMSTs (\code{"RMSTD"}) or the ratio between RMSTs (\code{"RMSTR"})
+#' @param output A string specifying whether to calculate the difference in RMSTs (\code{"RMSTD"}) or the ratio between RMSTs (\code{"RMSTR"}).
 #' @param HR A scalar specifying a hazard ratio. \code{HR} \eqn{< 1} suggests a lower hazard in the treatment group than in the control group.
-#' @param tau time horizon at which to evaluate RMST with \eqn{\mathrm{RMST} = \int_{0}^{\tau}S(t) \,dt}
+#' @param tau A scalar speifying the time horizon \eqn{\tau} at which to evaluate RMST with \eqn{\mathrm{RMST} = \int_{0}^{\tau}S(t) \,dt}.
+#' @param t A scalar specifying the time \eqn{t} at which to evaluate \code{survival_diff}.
+#' @param extended_output
 #'
 #' @return Returns either the difference (RMSTD) or ratio (RMSTR) in between RMSTs of treatment group and control group. An RMSTD \eqn{> 0} or an RMSTR \eqn{> 1} indicate a larger RMST in the treatment group.
 #' @export
@@ -30,7 +32,7 @@
 #' RMSTD <- convert_contrast(2)
 convert_contrast <- function(scale_trt = NULL, shape_trt = 1, scale_ctrl = NULL, shape_ctrl = 1,
                              parameterization = 1, HR = NULL, median_diff = NULL, percentile_diff = NULL, percentile = NULL,
-                             survival_diff = NULL, output = "RMSTD", tau = NULL, extended_output = F){
+                             survival_diff = NULL, t = NULL, output = "RMSTD", tau = NULL, extended_output = F){
   # convert from HR
 
   # throws errors if inputs are misspecified
@@ -62,40 +64,53 @@ convert_contrast <- function(scale_trt = NULL, shape_trt = 1, scale_ctrl = NULL,
   if(!is.null(HR)){
     if(!is.null(scale_trt)){
       scale_ctrl <- scale_trt*HR
-      print("first loop")}
-    else{
+      } else{
       scale_trt <- scale_ctrl/HR
-      print("2nd loop")}
-    print(paste("scale_trt is", scale_trt))
-    print(paste("shape_trt is", shape_trt))
-    RMST_trt <- integrate(pweibull, shape = shape_trt, scale = scale_trt, lower = 0,
-                          upper = tau, lower.tail = F)$value
-    RMST_ctrl <- integrate(pweibull, shape = shape_ctrl, scale = scale_ctrl, lower = 0,
-                          upper = tau, lower.tail = F)$value
+    }
   }
 
   if(!is.null(median_diff)){
-    if(!is.null(scale_trt))
-    median_time_trt <- -log(0.5)^(1/shape_trt)*scale_trt
-    median_time_ctrl <- median_time_trt - median_diff
-    scale_ctrl <- median_time_ctrl*(-log(0.5))^(-1/shape_ctrl)
-    else
-    median_time_ctrl <- -log(0.5)^(1/shape_ctrl)*scale_ctrl
-    median_time_trt <- median_time_ctrl + median_diff
-    scale_trt <- median_time_trt*(-log(0.5))^(-1/shape_trt)
+    if(!is.null(scale_trt)){
+      median_time_trt <- -log(0.5)^(1/shape_trt)*scale_trt
+      median_time_ctrl <- median_time_trt - median_diff
+      scale_ctrl <- median_time_ctrl*(-log(0.5))^(-1/shape_ctrl)
+      } else{
+      median_time_ctrl <- -log(0.5)^(1/shape_ctrl)*scale_ctrl
+      median_time_trt <- median_time_ctrl + median_diff
+      scale_trt <- median_time_trt*(-log(0.5))^(-1/shape_trt)
+      }
   }
 
   if(!is.null(percentile_diff)){
-    if(!is.null(scale_trt))
+    if(!is.null(scale_trt)){
       median_time_trt <- -log(percentile)^(1/shape_trt)*scale_trt
-    median_time_ctrl <- median_time_trt - median_diff
-    scale_ctrl <- median_time_ctrl*(-log(percentile))^(-1/shape_ctrl)
-    else
+      median_time_ctrl <- median_time_trt - median_diff
+      scale_ctrl <- median_time_ctrl*(-log(percentile))^(-1/shape_ctrl)
+      } else{
       median_time_ctrl <- -log(percentile)^(1/shape_ctrl)*scale_ctrl
-    median_time_trt <- median_time_ctrl + median_diff
-    scale_trt <- median_time_trt*(-log(percentile))^(-1/shape_trt)
+      median_time_trt <- median_time_ctrl + median_diff
+      scale_trt <- median_time_trt*(-log(percentile))^(-1/shape_trt)
+    }
   }
 
+  if(!is.null(survival_diff)){
+    if(!is.null(scale_trt)){
+      survival_trt <- pweibull(t, scale_trt, shape_trt, lower.tail = F)
+      survival_ctrl <- survival_trt - survival_diff
+      if(survival_ctrl <= 0){stop("survival in treatment group minus survival difference is equal to or less than zero")}
+      scale_ctrl <- t*(-log(survival_ctrl))^(-1/shape_ctrl)
+    } else{
+      survival_ctrl <- pweibull(t, scale_ctrl, shape_ctrl, lower.tail = F)
+      survival_trt <- survival_ctrl + survival_diff
+      if(survival_trt <= 0){stop("survival in control group minus survival difference is equal to or less than zero")}
+      scale_trt <- t*(-log(survival_trt))^(-1/shape_trt)
+    }
+  }
+
+  RMST_trt <- integrate(pweibull, shape = shape_trt, scale = scale_trt, lower = 0,
+                        upper = tau, lower.tail = F)$value
+  RMST_ctrl <- integrate(pweibull, shape = shape_ctrl, scale = scale_ctrl, lower = 0,
+                         upper = tau, lower.tail = F)$value
 
   if(output == "RMSTD"){return(RMST_trt-RMST_ctrl)}
   if(output == "RMSTR"){return(RMST_trt/RMST_ctrl)}
