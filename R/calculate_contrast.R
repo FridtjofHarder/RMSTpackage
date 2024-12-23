24625e37a091445c74a35645b932bfe0ebea40b8
FridtjofHarder <harder.fridtjof@gmail.com>
2024-12-24 00:52:28 +0100

    c

--- R/calculate_contrast.R
+++ R/calculate_contrast.R
@@ -4,7 +4,8 @@
 #' functions defined by scale and shape.
 #'
 #' Survival curves need to be defined by \dfn{scale} and \dfn{shape} parameter, in the standard parameterization
-#' defined by \eqn{S(t) = 1- F(t) = \exp{(-(t/\mathrm{scale})^\mathrm{shape}))}}. Possible contrasts are: \itemize{
+#' defined by \eqn{S(t) = 1- F(t) = \exp{(-(t/\mathrm{scale})^\mathrm{shape}))}}.
+#' Possible contrasts are: \itemize{
 #' \item Restricted mean survival time difference (RMSTD)
 #' \item Restricted mean survival time ratio (RMSTR)
 #' \item Hazard ratio (HR)
@@ -13,20 +14,18 @@
 #' \item Survival difference at time \eqn{t}}
 #'
 #' @param scale_trmt A scalar \eqn{>0} specifying the \dfn{scale parameter} in the treatment group.
-#' @param shape_trmt A scalar \eqn{>0} specifying the \dfn{shape parameter} in the treatment group. Defaults to \code{shape_trmt} \eqn{=1}, simplifying to exponential survival.
+#' @param shape_trmt A scalar \eqn{>0} specifying the \dfn{shape parameter} in the treatment group.
+#' Defaults to \code{shape_trmt} \eqn{=1}, simplifying to exponential survival.
 #' @param scale_ctrl A scalar \eqn{>0} specifying the \dfn{scale parameter} in the treatment group.
-#' @param shape_ctrl A scalar \eqn{>0} specifying the \dfn{shape parameter} in the treatment group. Defaults to \code{shape_ctrl} \eqn{=1}, simplifying to exponential survival.
+#' @param shape_ctrl A scalar \eqn{>0} specifying the \dfn{shape parameter} in the treatment group.
+#' Defaults to \code{shape_ctrl} \eqn{=1}, simplifying to exponential survival.
 #' @param parameterization One of: \itemize{
-#' \item \code{parameterization = 1}: specifies Weibull distributed survival as \eqn{S(t) = 1- F(t) = \exp{(-(t/\mathrm{scale})^\mathrm{shape}))}},
-#' \item \code{parameterization = 2}: specifies Weibull distributed survival as \eqn{S(t) = 1- F(t) = \exp{(-\mathrm{scale} * t^\mathrm{shape})}},
-#' \item \code{parameterization = 3}: specifies Weibull distributed survival as \eqn{S(t) = 1- F(t) = \exp{(-(\mathrm{scale} * t)^\mathrm{shape})}}.}
-#' @param output A string specifying whether to calculate the difference in
-#' RMSTs (\code{"RMSTD"}), the ratio between RMSTs (\code{"RMSTR"}), the HR
-#' (\code{"HR"}), the median difference (\code{"median_diff"}), the difference
-#' in time (\code{"percentile_diff"}) for reaching a survival percentile as
-#' specified in (\code{percentile}), and the survival difference (\code{"survival_diff"})
-#' at time specified in \code{t}.
-#' (\code{"HR"}).
+#' \item \code{parameterization = 1}: specifies Weibull distributed survival as
+#' \eqn{S(t) = 1- F(t) = \exp{(-(t/\mathrm{scale})^\mathrm{shape}))}},
+#' \item \code{parameterization = 2}: specifies Weibull distributed survival as
+#' \eqn{S(t) = 1- F(t) = \exp{(-\mathrm{scale} * t^\mathrm{shape})}},
+#' \item \code{parameterization = 3}: specifies Weibull distributed survival as
+#' \eqn{S(t) = 1- F(t) = \exp{(-(\mathrm{scale} * t)^\mathrm{shape})}}.}
 #' @param percentile A scalar specifying the percentile at which to evaluate the
 #' percentile difference.
 #' @param t A scalar specifying the time at which to evaluate the survival difference.
@@ -42,7 +41,6 @@
 #' calculate_contrast(scale_trmt = 2,
 #' scale_ctrl = 1,
 #' parameterization = 1,
-#' output = "RMSTD",
 #' percentile = NULL,
 #' t = NULL,
 #' tau = 1,
@@ -52,57 +50,66 @@ calculate_contrast <- function(scale_trmt = NULL,
                                scale_ctrl = NULL,
                                shape_ctrl = 1,
                                parameterization = 1,
-                               output = "RMSTD",
                                percentile = NULL,
                                t = NULL,
                                tau = NULL,
                                plot_curves = TRUE){
-# error management --------------------------------------------------------
+# error management -------------------------------------------------------------
 
-  stopifnot( # throw error when output misspecified
-    "output misspecified" = output == "RMSTD" ||
-      output == "RMSTR" || output == "HR" || output == "median_diff" || output == "percentile_diff"
-    || output == "survival_diff"
-  )
   stopifnot( # throw error when parameterization misspecified
-    "parameterization must be defined as either 1, 2, or 3" = parameterization == 1 ||
-      parameterization == 2 || parameterization == 3
+    "parameterization must be defined as either 1, 2, or 3" =
+      parameterization == 1 || parameterization == 2 || parameterization == 3
   )
-  # throw error when functions misspecified
-  if(is.null(scale_trmt)||is.null(scale_ctrl)){
-    stop("please specify scale parameters for both treatment and survival group")
+  # throw error when functions are misspecified
+  if (is.null(scale_trmt) || is.null(scale_ctrl)) {
+    stop("please specify scale parameters in both treatment and survival group")
   }
 
-  # throw error when tau should have been specified
-  if((output == "all" || output == "RMSTR" || output == "RMSTD") &&  !is.numeric(tau)){
-    stop("please specify valid time horizon tau")
-  }
+# adjust scale parameter if parameterization != 1-------------------------------
 
-  # throw error when percentile should have been specified
-  if(output == "all" || output == "percentine_diff")
-    stop("please specify valid percentile at which to evaluate percentile difference"
-         = !is.numeric(percentile))
-
-  # throw error when t should have been specified
-  if(output == "all" || output == "survival_diff")
-    stop("please specify valid time t at which to evaluate survival difference"
-         = !is.numeric(t))
-
-# main function -----------------------------------------------------------
-
-  # converts scale parameter wrt parameterization
   if (parameterization == 2) {
     if (!is.null(scale_trmt)) scale_trmt <- 1 / (scale_trmt ^ (1 / shape_trmt))
     if (!is.null(scale_ctrl)) scale_ctrl <- 1 / (scale_ctrl ^ (1 / shape_ctrl))
   }
-  # converts scale parameter wrt parameterization
+
   if (parameterization == 3) {
     if (!is.null(scale_trmt)) scale_trmt <- 1 / scale_trmt
     if (!is.null(scale_ctrl)) scale_ctrl <- 1 / scale_ctrl
   }
 
-  if(output == "all" || output == "RMSTD" || output == "RMSTR"){
-    RMST_trmt <- stats::integrate( # calculate RMST for treatment curve
+# main function ----------------------------------------------------------------
+  browser()
+  # define weibull functions for treatment and control
+  weibull_ctrl <- function(q){
+    stats::pweibull(q, shape = shape_ctrl, scale = scale_ctrl,
+             lower.tail = FALSE)
+  }
+  weibull_trmt <- function(q){
+    stats::pweibull(q, shape = shape_trmt, scale = scale_trmt,
+             lower.tail = FALSE)
+  }
+
+  # median survival time and median survival time difference
+  median_surv_time_ctrl <- uniroot(function(q) {weibull_ctrl(q) - 0.5},
+                                   lower = 0, upper = 1000)$root
+  median_surv_time_trmt <- uniroot(function(q) {weibull_trmt(q) - 0.5},
+                                   lower = 0, upper = 1000)$root
+  median_surv_time_diff <- median_surv_time_trmt - median_surv_time_ctrl
+
+  # hazard ratio
+  if(shape_ctrl == 1) h_ctrl <- 1 / scale_ctrl else {
+    h_ctrl <-  function(t) get_weibull_h(t, shape_ctrl, scale_ctrl)
+  }
+  if(shape_trmt == 1) h_trmt <- 1 / scale_trmt else {
+    h_trmt <-  function(t) get_weibull_h(t, shape_trmt, scale_trmt)
+  }
+
+  h_trmt <-  function(t) get_weibull_h(t, shape_trmt, scale_trmt)
+  h_ratio <- function(t) h_trmt(t) / h_ctrl(t)
+
+  # calculate RMSTD and RMSTR for treatment and control curve
+  if (!is.null(tau)){
+    RMST_trmt <- stats::integrate(
       stats::pweibull,
       shape = shape_trmt,
       scale = scale_trmt,
@@ -122,27 +129,50 @@ calculate_contrast <- function(scale_trmt = NULL,
     RMSTR <- RMST_trmt / RMST_ctrl
   }
 
-  if(output == "all" || output == "RMSTD" || output == "RMSTR")
+  # hazard ratio
 
-  if(plot_curves){
+  # percentile survival
+
+  # pointwise survival difference
+
+  if (plot_curves) {
     x = NULL # to silence check() notes
-    graphics::curve(stats::pweibull(x, scale = scale_trmt, shape = shape_trmt, lower.tail = FALSE),
-          col = "green", xlab = "t", ylab = "S(t)", ylim = c(0, 1), xlim = c(0, 1.5*tau))
-    graphics::curve(stats::pweibull(x, scale = scale_ctrl, shape = shape_ctrl, lower.tail = FALSE),
-          col = "red", add = TRUE)
-    graphics::abline(v = tau, col = "blue")
-    #graphics::text(x = tau, y = 0.1, pos = 4, labels = paste("time horizon \u03C4  =", tau))
-    graphics::text(x = tau, y = 0.1, pos = 4, labels = bquote("time horizon " * tau * " = " * .(tau)))
-    graphics::legend("bottomleft", legend=c(paste0("treatment group with \n", "scale =",
-                                         round(scale_trmt, 2), " and shape =",
+    # plots treatment curve
+    graphics::curve(stats::pweibull(x, scale = scale_trmt, shape = shape_trmt,
+                                    lower.tail = FALSE),
+                    col = "darkblue", xlab = "t", ylab = "S(t)", lwd = 3,
+                    ylim = c(0, 1), xlim = c(0, 1.5*tau), las = 1)
+    # plots control curve
+    graphics::curve(stats::pweibull(x, scale = scale_ctrl, shape = shape_ctrl,
+                                    lower.tail = FALSE),
+                    col = "red", lwd = 3, add = TRUE)
+    graphics::abline(v = tau, col = "black", lwd = 3)
+    # mark tau if tau is defined
+    if (!is.null(tau)) {
+    graphics::text(x = tau, y = 0.1, pos = 4,
+                   labels = bquote("time horizon " * tau * " = " * .(tau)))
+    }
+    graphics::legend("bottomleft",
+                     legend = c(paste0("treatment group with \n", "scale = ",
+                                         round(scale_trmt, 2), " and shape = ",
                                          round(shape_trmt, 2)),
-                                  paste0("control group with \n", "scale =",
-                                         round(scale_ctrl, 2), " and shape =",
+                                paste0("control group with \n",   "scale = ",
+                                         round(scale_ctrl, 2), " and shape = ",
                                          round(shape_ctrl, 2))),
-           col=c("green", "red"), lty=1:1, y.intersp = 1.5, bty = "n", cex = 0.8)
-
-
+                     col=c("darkblue", "red"), lty = 1, y.intersp = 2,
+                     bty = "n", cex = 1)
+    graphics::title("Survival curves in control group and treatment group")
   }
   return(list("RMSTD" = RMSTD, "RMSTR" = RMSTR))
 }
 
+#' Hazard function for weibull distributed survival
+#'
+#' @param t time
+#' @param shape shape parameter
+#' @param scale scale parameter
+#'
+#' @returns
+get_weibull_h <- function(t, shape, scale){
+  return((shape / scale) * (t / scale)^(shape - 1))
+}
